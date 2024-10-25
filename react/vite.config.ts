c66aa202b3ae21ea4ca4b1fe5734329ed4ca0096
linhtran <linh.tran@saleshood.com>
2024-10-25 19:26:20 +0700

    chore: config vite to build assets served by server app

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild', // Use fast minification
+    outDir: '../server/assets/',
+    emptyOutDir: true, // Clears previous build files
+    rollupOptions: {
+        input: './index.html',
+        output: {
+            entryFileNames: 'assets/[name].[hash].js',
+            assetFileNames: 'assets/[name].[hash].[ext]'
+        }
+    },
+  },
   plugins: [react()],
 })
